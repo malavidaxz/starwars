c57267c2e0089d3a1bff25c0d3790a59f4a550f5
malavidaxz <110206463+malavidaxz@users.noreply.github.com>
2023-06-23 03:05:23 -0500

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PeopleComponent } from './dashboard/people/people.component';
+import { FilmsComponent } from './dashboard/films/films.component';
+import { PlanetsComponent } from './dashboard/planets/planets.component';
+import { SpeciesComponent } from './dashboard/species/species.component';
+import { StarshipsComponent } from './dashboard/starships/starships.component';
+import { VehiclesComponent } from './dashboard/vehicles/vehicles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    PeopleComponent,
+    FilmsComponent,
+    PlanetsComponent,
+    SpeciesComponent,
+    StarshipsComponent,
+    VehiclesComponent
   ],
   imports: [
     BrowserModule
